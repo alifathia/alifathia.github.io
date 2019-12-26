b50cdac10d1d6797474aeb28a1e01e62f0688a96
Alifa Fathia Khaerani <alifa.fathia@ui.ac.id>
2019-12-26 14:00:38 +0700

    init

---
+++ script.js
@@ -0,0 +1,35 @@
+function changeColor1() {
+    document.getElementById("textColor1").style.color = "#0a7c96";
+}
+
+function revertColor1() {
+    document.getElementById("textColor1").style.color = "#444";
+}
+
+function changeColor2() {
+    document.getElementById("textColor2").style.color = "#0a7c96";
+}
+
+function revertColor2() {
+    document.getElementById("textColor2").style.color = "#444";
+}
+
+function changeColor3() {
+    document.getElementById("textColor3").style.color = "#0a7c96";
+}
+
+function revertColor3() {
+    document.getElementById("textColor3").style.color = "#444";
+}
+
+function imageEnlarge() {
+    document.getElementById('img').style.height="25%";
+    document.getElementById('img').style.width="25%";
+    document.getElementById('img').style.position='relative';
+}
+
+function imageReset() {
+    document.getElementById('img').style.height="20%";
+    document.getElementById('img').style.width="20%";
+    document.getElementById('img').style.position='relative';
+}
